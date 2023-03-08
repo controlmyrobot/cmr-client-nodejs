66e0c2386e424e1efceb41117c0d4deedd4386f8
dtbaker <dtbaker@gmail.com>
2023-03-08 15:44:43 +1000

    Clean up after qr code scan

--- client/socket-video-jpeg.js
+++ client/socket-video-jpeg.js
@@ -1,13 +1,16 @@
 const {SocketBase} = require("./socket")
 const zbarimg = require('node-zbarimg')
-const fs = require('fs');
+const fs = require('fs')
 
 class SocketVideoJpeg extends SocketBase {
     clientType = 'SOCKET_VIDEO_JPEG'
 
     init() {
+        this.lookForQrCodes = true
         this.startGenericSocketPipe(data => {
-            this.maybeParseQrCode(data)
+            if(this.lookForQrCodes) {
+                this.maybeParseQrCode(data)
+            }
         })
     }
 
@@ -15,7 +18,7 @@ class SocketVideoJpeg extends SocketBase {
         // Look for QR code data every 2 seconds:
         if(!this.lastLookedForQrData || (Date.now() - this.lastLookedForQrData) > 2000){
             this.lastLookedForQrData = Date.now()
-            const tmpFilename = `${this.config.tmpDir}/still-image-for-qr.jpg`
+            const tmpFilename = `${this.config.tmpDir}/cmr-qr-code-${this.backend.robot_backend_id.replace(/[^a-zA-Z0-9]/g,'')}.jpg`
             fs.writeFileSync(tmpFilename,data)
             zbarimg(tmpFilename, (err, code) => {
                 if(code){
@@ -25,6 +28,7 @@ class SocketVideoJpeg extends SocketBase {
                         console.error('Found a QR code, but it does not start with CMR:')
                     }
                 }
+                fs.unlinkSync(tmpFilename)
             })
         }
     }
